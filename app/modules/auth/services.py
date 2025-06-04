058f96a733dc95303b3d863060e0b37ef122dddb
RehanAhmad13 <rehantheguy2008@hotmail.com>
2025-06-04 16:53:53 +0400

    Replace network script with auth repository

--- app/modules/auth/services.py
+++ app/modules/auth/services.py
@@ -1,31 +1,22 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from app.core.security import (
-    hash_password,
     verify_password,
     create_access_token,
     create_refresh_token,
 )
 from app.modules.auth.schemas import UserCreate
 from app.database.models import User
+from . import repository
 
 def create_user(user_in: UserCreate, db: Session):
-    if db.query(User).filter(User.email == user_in.email).first():
+    if repository.get_user_by_email(db, user_in.email):
         raise HTTPException(status_code=400, detail="Email already registered")
-    
-    new_user = User(
-        username=user_in.username,
-        email=user_in.email,
-        hashed_password=hash_password(user_in.password),
-        role=user_in.role.value,
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+
+    return repository.create_user(db, user_in)
 
 def authenticate_user(email: str, password: str, db: Session):
-    user = db.query(User).filter(User.email == email).first()
+    user = repository.get_user_by_email(db, email)
     if not user or not verify_password(password, user.hashed_password):
         return None
     return user
