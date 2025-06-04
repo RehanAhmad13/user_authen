28c4ae45e901019f6cf89bcb2f514b6ce3c4733e
Rehan Ahmad <rehantheguy2008@hotmail.com>
2025-06-04 15:22:52 +0400

    Initial commit: FastAPI JWT Auth system setup

---
+++ app/modules/auth/routers.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.database.dependency import get_db
+from app.modules.auth.schemas import UserCreate, UserOut, Token
+from app.modules.auth.services import create_user, authenticate_user
+from app.core.security import create_access_token
+from fastapi import Depends
+from app.shared.dependencies import get_current_user
+from app.modules.auth.schemas import UserOut
+
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/register", response_model=UserOut)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(user, db)
+
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/me", response_model=UserOut)
+def get_me(current_user: UserOut = Depends(get_current_user)):
+    return current_user
+
---
+++ app/modules/auth/services.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from app.core.security import hash_password, verify_password, create_access_token
+from app.modules.auth.schemas import UserCreate
+from app.database.models import User
+
+def create_user(user_in: UserCreate, db: Session):
+    if db.query(User).filter(User.email == user_in.email).first():
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    new_user = User(
+        username=user_in.username,
+        email=user_in.email,
+        hashed_password=hash_password(user_in.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def authenticate_user(email: str, password: str, db: Session):
+    user = db.query(User).filter(User.email == email).first()
+    if not user or not verify_password(password, user.hashed_password):
+        return None
+    return user
